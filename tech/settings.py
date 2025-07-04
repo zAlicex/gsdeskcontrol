502f973425f3bca42962f924b005a2ab4bdc5468
Alice Oliveira <aliceoliveira45130@gemail.com>
2025-07-04 11:03:52 -0300

    Alterações locais antes do pull

--- tech/settings.py
+++ tech/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'widget_tweaks',
     'accounts',
     'home',
     'clientes',
